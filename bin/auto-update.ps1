81febeab5e3bbe0c4c081881914aa53dfa6823d6
Ihor Sofiichenko <asoluter@gmail.com>
2022-11-18 10:20:23 +0200

    Update auto-update.ps1

--- bin/auto-update.ps1
+++ bin/auto-update.ps1
@@ -1,24 +1,32 @@
 <#
 .SYNOPSIS
-    Updates manifests and pushes them.
+    Updates manifests and pushes them or creates pull-requests.
 .DESCRIPTION
-    Updates manifests and pushes them directly to the main branch.
+    Updates manifests and pushes them directly to the origin branch or creates pull-requests for upstream.
 .PARAMETER Upstream
     Upstream repository with the target branch.
     Must be in format '<user>/<repo>:<branch>'
+.PARAMETER OriginBranch
+    Origin (local) branch name.
 .PARAMETER App
     Manifest name to search.
     Placeholders are supported.
+.PARAMETER CommitMessageFormat
+    The format of the commit message.
+    <app> will be replaced with the file name of manifest.
+    <version> will be replaced with the version of the latest manifest.
 .PARAMETER Dir
     The directory where to search for manifests.
 .PARAMETER Push
-    Push updates directly to 'origin main'.
+    Push updates directly to 'origin branch'.
 .PARAMETER Help
     Print help to console.
 .PARAMETER SpecialSnowflakes
     An array of manifests, which should be updated all the time. (-ForceUpdate parameter to checkver)
 .PARAMETER SkipUpdated
     Updated manifests will not be shown.
+.PARAMETER ThrowError
+    Throw error as exception instead of just printing it.
 .EXAMPLE
     PS BUCKETROOT > .\bin\auto-pr.ps1 'someUsername/repository:branch' -Request
 .EXAMPLE
@@ -33,8 +41,10 @@ param(
         }
         $true
     })]
-    [String] $Upstream = "asoluter/Scoop-Asoluter:main",
+    [String] $Upstream = 'asoluter/Scoop-Asoluter:main',
+    [String] $OriginBranch = 'main',
     [String] $App = '*',
+    [String] $CommitMessageFormat = '<app>: Update to version <version>',
     [ValidateScript( {
         if (!(Test-Path $_ -Type Container)) {
             throw "$_ is not a directory!"
@@ -42,22 +52,48 @@ param(
             $true
         }
     })]
-    [String] $Dir = "$psscriptroot/../bucket",
+    [String] $Dir = "$PSScriptRoot/../bucket",
+    #[Switch] $Push,
+    [Switch] $Help,
     [string[]] $SpecialSnowflakes,
-    [Switch] $SkipUpdated
+    [Switch] $SkipUpdated,
+    [Switch] $ThrowError
 )
 
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+$Push = $true
+$Request = $false
 
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 . "$env:SCOOP_HOME\lib\manifest.ps1"
 . "$env:SCOOP_HOME\lib\json.ps1"
-. "$env:SCOOP_HOME\lib\unix.ps1"
 
-$Dir = Resolve-Path $Dir
+if ($App -ne '*' -and (Test-Path $App -PathType Leaf)) {
+    $Dir = Split-Path $App
+} elseif ($Dir) {
+    $Dir = Convert-Path $Dir
+} else {
+    throw "'-Dir' parameter required if '-App' is not a filepath!"
+}
+
+if ((!$Push -and !$Request) -or $Help) {
+    Write-Host @'
+Usage: auto-pr.ps1 [OPTION]
+
+Mandatory options:
+  -p,  -push                       push updates directly to 'origin branch'
+  -r,  -request                    create pull-requests on 'upstream branch' for each update
+
+Optional options:
+  -u,  -upstream                   upstream repository with target branch
+  -o,  -originbranch               origin (local) branch name
+  -h,  -help
+'@
+    exit 0
+}
 
 function execute($cmd) {
     Write-Host $cmd -ForegroundColor Green
-    $output = Invoke-Expression $cmd
+    $output = Invoke-Command ([scriptblock]::Create($cmd))
 
     if ($LASTEXITCODE -gt 0) {
         abort "^^^ Error! See above ^^^ (last command: $cmd)"
@@ -67,14 +103,19 @@ function execute($cmd) {
 }
 
 Write-Host 'Updating ...' -ForegroundColor DarkCyan
-execute 'git pull origin main'
-execute 'git checkout main'
+if ($Push) {
+    execute "git pull origin $OriginBranch"
+    execute "git checkout $OriginBranch"
+} else {
+    execute "git pull upstream $OriginBranch"
+    execute "git push origin $OriginBranch"
+}
 
-. "$env:SCOOP_HOME\bin\checkver.ps1" -App $App -Dir $Dir -Update -SkipUpdated:$SkipUpdated
+. "$env:SCOOP_HOME\bin\checkver.ps1" -App $App -Dir $Dir -Update -SkipUpdated:$SkipUpdated -ThrowError:$ThrowError
 if ($SpecialSnowflakes) {
     Write-Host "Forcing update on our special snowflakes: $($SpecialSnowflakes -join ',')" -ForegroundColor DarkCyan
     $SpecialSnowflakes -split ',' | ForEach-Object {
-        . "$env:SCOOP_HOME\bin\checkver.ps1" $_ -Dir $Dir -ForceUpdate
+        . "$env:SCOOP_HOME\bin\checkver.ps1" $_ -Dir $Dir -ForceUpdate -ThrowError:$ThrowError
     }
 }
 
@@ -91,21 +132,30 @@ git diff --name-only | ForEach-Object {
         return
     }
     $version = $json.version
+    $CommitMessage = $CommitMessageFormat -replace '<app>',$app -replace '<version>',$version
+    if ($Push) {
+        Write-Host "Creating update $app ($version) ..." -ForegroundColor DarkCyan
+        execute "git add $manifest"
 
-    Write-Host "Creating update $app ($version) ..." -ForegroundColor DarkCyan
-    execute "git add $manifest"
-
-    # detect if file was staged, because it's not when only LF or CRLF have changed
-    $status = execute 'git status --porcelain -uno'
-    $status = $status | Where-Object { $_ -match "M\s{2}.*$app.json" }
-    if ($status -and $status.StartsWith('M  ') -and $status.EndsWith("$app.json")) {
-        execute "git commit -m '${app}: Update to version $version'"
+        # detect if file was staged, because it's not when only LF or CRLF have changed
+        $status = execute 'git status --porcelain -uno'
+        $status = $status | Where-Object { $_ -match "M\s{2}.*$app.json" }
+        if ($status -and $status.StartsWith('M  ') -and $status.EndsWith("$app.json")) {
+            execute "git commit -m '$commitMessage'"
+        } else {
+            Write-Host "Skipping $app because only LF/CRLF changes were detected ..." -ForegroundColor Yellow
+        }
     } else {
-        Write-Host "Skipping $app because only LF/CRLF changes were detected ..." -ForegroundColor Yellow
+        pull_requests $json $app $Upstream $manifest $CommitMessage
     }
 }
 
-Write-Host 'Pushing updates ...' -ForegroundColor DarkCyan
-execute 'git push origin main'
+if ($Push) {
+    Write-Host 'Pushing updates ...' -ForegroundColor DarkCyan
+    execute "git push origin $OriginBranch"
+} else {
+    Write-Host "Returning to $OriginBranch branch and removing unstaged files ..." -ForegroundColor DarkCyan
+    execute "git checkout -f $OriginBranch"
+}
 
 execute 'git reset --hard'
